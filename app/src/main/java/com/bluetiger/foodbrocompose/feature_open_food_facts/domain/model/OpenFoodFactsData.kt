44e3297f013751101aa638dff55f09156a7e2352
Malte Kanders <92604963+BTBlueTiger@users.noreply.github.com>
2023-09-07 13:06:11 +0200

    Refactored OpenFoodFactData to use user as foreign key

--- app/src/main/java/com/bluetiger/foodbrocompose/feature_open_food_facts/domain/model/OpenFoodFactsData.kt
+++ app/src/main/java/com/bluetiger/foodbrocompose/feature_open_food_facts/domain/model/OpenFoodFactsData.kt
@@ -1,20 +1,32 @@
 package com.bluetiger.foodbrocompose.feature_open_food_facts.domain.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.bluetiger.foodbrocompose.feature_open_food_facts.domain.model.ecoscore.EcoScoreData
 import com.bluetiger.foodbrocompose.feature_open_food_facts.domain.model.nutrient_levels.NutrientLevels
 import com.bluetiger.foodbrocompose.feature_open_food_facts.domain.model.nutriments.Nutriments
 import com.bluetiger.foodbrocompose.feature_open_food_facts.domain.model.nutriscore_data.NutriScoreData
 import com.bluetiger.foodbrocompose.feature_open_food_facts.domain.model.product_general.ProductGeneral
+import com.bluetiger.foodbrocompose.feature_user.domain.model.User
 
-@Entity(tableName = "OpenFoodFactsResponses")
+@Entity(
+    tableName = "OpenFoodFactsResponses",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["name"],
+            childColumns = ["userName"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class OpenFoodFactsData(
-    val status: Int = 0,
-    val barcode: String = "",
     @PrimaryKey
     val timeStamp: Long = System.currentTimeMillis(),
-    val userMail: String = "",
+    val status: Int = 0,
+    val barcode: String = "",
+    val userName: String = "",
     val nutriScoreData: NutriScoreData? = null,
     val ecoScoreData: EcoScoreData? = null,
     val productGeneral: ProductGeneral? = null,
