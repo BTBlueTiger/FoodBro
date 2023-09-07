200d9344cd9a94304d8f7cd0abda5860a8e3f9d8
Malte Kanders <92604963+BTBlueTiger@users.noreply.github.com>
2023-09-07 12:25:35 +0200

    Implemented base structure

---
+++ app/src/main/java/com/bluetiger/foodbrocompose/feature_user_list/data/repository/FoodBroListRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.bluetiger.foodbrocompose.feature_user_list.data.repository
+
+import com.bluetiger.foodbrocompose.feature_user.domain.model.User
+import com.bluetiger.foodbrocompose.feature_user_list.data.data_source.FoodBroListDao
+import com.bluetiger.foodbrocompose.feature_user_list.domain.model.FoodBroList
+import com.bluetiger.foodbrocompose.feature_user_list.domain.repository.FoodBroListRepository
+import kotlinx.coroutines.flow.Flow
+
+class FoodBroListRepositoryImpl(private val dao: FoodBroListDao) : FoodBroListRepository() {
+    override fun getFoodBroListsByUser(user: User): Flow<List<FoodBroList>> {
+        return dao.getFoodBroListByUser(user.email)
+    }
+
+    override suspend fun insertFoodBroList(foodBroList: FoodBroList) {
+        dao.insert(foodBroList)
+    }
+
+    override suspend fun deleteFoodBroList(foodBroList: FoodBroList) {
+        dao.delete(foodBroList)
+    }
+}
